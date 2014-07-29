784c7c34d96c03ce0666dcc4997a2f11f886cdec
cmew3 <c.wheeler-booth@cantab.net>
2014-07-29 15:29:09 +0100

    moved lib file into app/models

--- app/server.rb
+++ app/server.rb
@@ -1,14 +1,14 @@
 require 'data_mapper'
 require 'sinatra'
-require './lib/link' #this needs to be done after datamapper is initialised
-require './lib/tag'
-require './lib/user'
+require './app/models/link' #this needs to be done after datamapper is initialised
+require './app/models/tag'
+require './app/models/user'
 require 'rack-flash'
 require 'sinatra/partial'
 require_relative 'helpers/application'
 require_relative 'data_mapper_setup'
 require_relative 'controllers/sessions'
-require_relative 'controllers/users'
+require_relative 'controllers/users'	
 require_relative 'controllers/tags'
 require_relative 'controllers/links'
 require_relative 'controllers/application'
