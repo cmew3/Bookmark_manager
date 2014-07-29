4cb501e5735e409b5c51e301b142ff693a4ba92d
cmew3 <c.wheeler-booth@cantab.net>
2014-07-29 14:34:35 +0100

    refactored layout (using partials) and server.rb into controllers

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'sinatra'
 gem 'dm-postgres-adapter'
 gem 'bcrypt-ruby'
 gem 'rack-flash3'
+gem 'sinatra-partial'
 
 group :development, :test do
   gem "rspec"
--- spec/features/user_management_spec.rb
+++ spec/features/user_management_spec.rb
@@ -1,4 +1,7 @@
 require 'spec_helper'
+require_relative 'helpers/session'
+
+include SessionHelpers
 
 feature 'User signs up' do
 
@@ -22,17 +25,47 @@ feature 'User signs up' do
 	end
 
 
+end
 
-	def sign_up(email = 'alice@example.com', 
-				password = 'oranges!',
-				password_confirmation = 'oranges!')
-		visit '/users/new'
-		# expect(page.status_code).to eq(200)
-		# expect(page.status_code).to eq(200)
-		fill_in :email, :with => email
-		fill_in :password, :with => password
-		fill_in :password_confirmation, :with => password_confirmation
-		click_button "Sign up"
+feature 'user signs in' do
+
+	before(:each) do
+		User.create(:email => "test@test.com",
+					:password => "test",
+					:password_confirmation => "test")
+	end
+
+	scenario 'with correct credentials' do
+		visit '/'
+		expect(page).not_to have_content("Welcome, test@test.com")
+		sign_in('test@test.com','test')
+		expect(page).to have_content("Welcome, test@test.com")
+	end
+
+	scenario 'with incorrect credentials' do
+		visit '/'
+		expect(page).not_to have_content("Welcome, test@test.com")
+		sign_in('test@test.com','not_password')
+		expect(page).not_to have_content("Welcome, test@test.com")
+	end
+
+
+end
+
+feature 'user signs out' do
+
+	before(:each) do
+		User.create(:email => "test@test.com",
+					:password => "test",
+					:password_confirmation => "test")
+	end
+
+	scenario 'while being signed in' do
+		sign_in('test@test.com','test')
+		click_button "Sign out"
+		expect(page).to have_content("Goodbye!")
+		expect(page).not_to have_content("Welcome, test@test.com")
 	end
 
 end
+
